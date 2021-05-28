f5dcd970caaff070c807290db01ca6ac669a65c3
Nazim Shaikh <70259398+Nazim-shaikh@users.noreply.github.com>
2021-05-28 14:53:54 +0530

    Add files via upload

---
+++ seller.js
@@ -0,0 +1,84 @@
+messages = document.getElementById('messages')
+input = document.getElementById('input')
+sellerblock = document.getElementById('sellerblock')
+buyerblock = document.getElementById('buyerblock')
+
+msgno = 0
+
+products = {
+"mobile":{price:12000},
+"tomato":{price:30},
+"rice":{price:60},
+"book":{price:500},
+"laptop":{price:25000},
+"chair":{price:350}
+}
+
+
+function taketheinput(event) {
+	// here is the js code for input processing
+	//if they hit the enter key 
+	if(event.key === "Enter"){
+// create a buyer message block
+		messages.innerHTML += buyerblock.outerHTML
+		// and change its id
+		msgno += 1
+		messages.lastChild.id = msgno
+		//now changing its text
+		messages.lastChild.childNodes[1].textContent = input.value
+		// finally process the input 
+		processinput(input.value.toLowerCase())
+		input.value = ""
+
+	}
+}
+
+function processinput(inputval){
+
+	if(inputval!=""){
+		messages.innerHTML += sellerblock.outerHTML
+		// and change its id
+		msgno += 1
+		messages.lastChild.id = msgno
+		//now changing its text
+		messages.lastChild.childNodes[1].textContent = reply(inputval)	
+	}
+
+}
+
+
+
+function reply(inputval) {
+	result = inputval.match(/(how)|(\w+)/g)
+
+	if(result.includes("how")){
+		return "fine" 
+	}
+	if(result.includes("fund")){
+		return "To receive Payment Acknowledgement please follow these instructions: a]. If you have used direct payment method i.e Debit Card/Credit Card or Net banking, then you need to login to your profile using the email ID used to make the transaction and download it from the donation history section. You can click on forgot password to retrieve your password. b]. If you have used UPI/Bank transfer/PayTM Wallet payment method, you need to visit on the fundraiser page to which you have supported and look out for click here to receive payment acknowledgement option and fill up the necessary details in order to receive payment acknowledgement. c]. For 80G Certificate please follow the below instructions: Link to download the certificate- d]. Also, check your inbox & spam box, you might have received an email from Ketto with the attachment of the same when you completed your transaction. If you are still unable to find it, please drop us an email on info@funded"
+		
+	}
+	if(result.includes("hi")){
+		return "hi"
+	}
+	if(result.includes("price")){
+		return recentproduct.price
+	}
+	
+	a="";
+	result.forEach(function(product){
+		if(products.hasOwnProperty(product)){
+			a = "Yes we have " + product
+			recentproduct = products[product]
+		}
+	})
+	if(a){
+		return a;
+	}
+
+
+	return "Sorry " + inputval + " is not available!"
+	// if (inputval === "How are you?") {
+	// 	return "I am fine"
+	// }
+}
